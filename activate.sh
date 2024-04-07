6e0ff1618fc0d749ab78d73a741eb8077eb48240
Qwerasd <qwerasd205@users.noreply.github.com>
2024-04-06 22:50:33 -0400

    venv

--- activate.sh
+++ activate.sh
@@ -17,5 +17,6 @@ echo ""
 echo "================"
 echo ""
 echo "Done setting up virtual environment."
+echo "Use the venv in your shell by calling \`source .venv/bin/activate\`, or \`. .venv/bin/activate.fish\` for fish shell."
 echo "Deactivate with \`deactivate\`."
 
